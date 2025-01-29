2e23ce4844170a6a1c94d02a415ae8c783dc706c
Fabian Villca <fabian.villca@educa.madrid.org>
2025-01-29 18:54:41 +0100

    Commit FINAL del EJERCICIO1

--- Maui_app/MainPage.xaml.cs
+++ Maui_app/MainPage.xaml.cs
@@ -3,6 +3,7 @@
     public partial class MainPage : ContentPage
     {
         string translatedNumber;
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,31 +24,32 @@
                 CallButton.IsEnabled = false;
                 CallButton.Text = "Call";
             }
-            async void OnCall(object sender, System.EventArgs e)
+        }
+
+        // Mueve el método OnCall fuera de OnTranslate
+        private async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
             {
-                if (await this.DisplayAlert(
-                    "Dial a Number",
-                    "Would you like to call " + translatedNumber + "?",
-                    "Yes",
-                    "No"))
+                try
                 {
-                    try
-                    {
-                        if (PhoneDialer.Default.IsSupported)
-                            PhoneDialer.Default.Open(translatedNumber);
-                    }
-                    catch (ArgumentNullException)
-                    {
-                        await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
-                    }
-                    catch (Exception)
-                    {
-                        // Other error has occurred.
-                        await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
-                    }
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
                 }
             }
         }
     }
 }
-    

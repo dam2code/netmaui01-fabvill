520fc4f71a0c7e3145f85d6e694884ad193594e2
Fabian Villca <fabian.villca@educa.madrid.org>
2025-02-09 23:34:26 +0100

    Commit FINAL Uso de SQLite de forma asincrónica EJERCICIO7

--- MauiApp7/MainPage.xaml.cs
+++ MauiApp7/MainPage.xaml.cs
@@ -3,10 +3,26 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        private object statusMessage;
+        private object peopleList;
 
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public async void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+            await App.PersonRepo.AddNewPerson(newPerson.Text);
+            statusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+
+        public async void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+            List<Person> people = await App.PersonRepo.GetAllPeople();
+            peopleList.ItemsSource = people;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -21,5 +37,4 @@
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
-
 }
--- MauiApp7/PersonRepository.cs
+++ MauiApp7/PersonRepository.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using SQLite;  
 using MauiApp7.Models;  // Asegúrate de que la clase Person esté en este espacio de nombres
+using System.Threading.Tasks;
 
 namespace MauiApp7
 {
     public class PersonRepository
     {
         string _dbPath;
-
         public string StatusMessage { get; set; }
 
-        private SQLiteConnection conn;
 
         // TODO: Add variable for the SQLite connection
 
-        private void Init()
+        private SQLiteAsyncConnection conn;
+
+        private async Task Init()
         {
             if (conn != null)
                 return;
 
-            conn = new SQLiteConnection(_dbPath);
-            conn.CreateTable<Person>();
+            conn = new SQLiteAsyncConnection(_dbPath);
+
+            await conn.CreateTableAsync<Person>();
         }
 
         public PersonRepository(string dbPath)
@@ -29,36 +31,34 @@ namespace MauiApp7
             _dbPath = dbPath;
         }
 
-        public void AddNewPerson(string name)
+        public async Task AddNewPerson(string name)
         {
             int result = 0;
             try
             {
-                // enter this line
-                Init();
+                // Call Init()
+                await Init();
 
                 // basic validation to ensure a name was entered
                 if (string.IsNullOrEmpty(name))
                     throw new Exception("Valid name required");
 
-                // enter this line
-                result = conn.Insert(new Person { Name = name });
+                result = await conn.InsertAsync(new Person { Name = name });
 
-                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
             }
             catch (Exception ex)
             {
                 StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
             }
-
         }
 
-        public List<Person> GetAllPeople()
+        public async Task<List<Person>> GetAllPeople()
         {
             try
             {
-                Init();
-                return conn.Table<Person>().ToList();
+                await Init();
+                return await conn.Table<Person>().ToListAsync();
             }
             catch (Exception ex)
             {

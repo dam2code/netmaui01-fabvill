c55641f57c9c70d919616a33644caf5afa4ca411
Fabian Villca <fabian.villca@educa.madrid.org>
2025-02-13 12:55:17 +0100

    Commit FINAL Conversión de un controlador de eventos en un comando EJERCICIO9

--- MauiApp9/ViewModels/MovieListViewModel.cs
+++ MauiApp9/ViewModels/MovieListViewModel.cs
@@ -4,15 +4,20 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiApp9.ViewModels
 {
     public class MovieListViewModel : ObservableObject
     {
+        public ICommand DeleteMovieCommand { get; private set; }
         public ObservableCollection<MovieViewModel> Movies { get; set; }
 
-        public MovieListViewModel() =>
+        public MovieListViewModel()
+        {
             Movies = [];
+            DeleteMovieCommand = new Command<MovieViewModel>(DeleteMovie);
+        }
 
         public async Task RefreshMovies()
         {
@@ -24,5 +29,12 @@ namespace MauiApp9.ViewModels
 
         public void DeleteMovie(MovieViewModel movie) =>
             Movies.Remove(movie);
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            ViewModels.MovieViewModel movie = (ViewModels.MovieViewModel)menuItem.BindingContext;
+            App.MainViewModel.DeleteMovie(movie);
+        }
     }
+
 }
